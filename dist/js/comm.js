80418a4529da00c9a3447c3f2f86100de567a355
miloupark <helenapark96@naver.com>
2023-11-06 18:13:45 +0900

    comm

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -87,14 +87,13 @@ gsap.fromTo(".profile-intro-contents-star", {
 });
 
 // Section 3 : left descirption / tokenpost-num
-gsap.to(".tokenpost-num, .mendini-num, .verve-num", {
+gsap.to(".sec-left-icon", {
   rotation: 360,
   duration: 2,
   repeat: -1,
   repeatDelay: 3,
   ease: 'none'
 });
-
 
 gsap.fromTo(".shakingeyes", {
   x: 350,
@@ -105,3 +104,14 @@ gsap.fromTo(".shakingeyes", {
   yoyo: true,
   repeat: -1, 
 });
+
+// Section : left descirption / sec-left-arrows
+gsap.to(".sec-left-arrows-txt", {
+  duration: 0.3,
+  y: 10,
+  ease: "circ.in",
+  repeat: -1,
+  yoyo: true,
+});
+
+
