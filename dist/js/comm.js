0a1b9d65a0cc6fe152e0025bde48dad9a3d8cfc7
miloupark <helenapark96@naver.com>
2023-11-07 10:24:58 +0900

    icon remove

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -87,13 +87,13 @@ gsap.fromTo(".profile-intro-contents-star", {
 });
 
 // Section 3 : left descirption / tokenpost-num
-gsap.to(".sec-left-icon", {
-  rotation: 360,
-  duration: 2,
-  repeat: -1,
-  repeatDelay: 3,
-  ease: 'none'
-});
+// gsap.to(".sec-left-icon", {
+//   rotation: 360,
+//   duration: 2,
+//   repeat: -1,
+//   repeatDelay: 3,
+//   ease: 'none'
+// });
 
 gsap.fromTo(".shakingeyes", {
   x: 350,
